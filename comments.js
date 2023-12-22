cfcc2e8e999351e01c3466562356080500164d0d
github-cloudlabsuser-264 <150386090+github-cloudlabsuser-264@users.noreply.github.com>
2023-12-22 18:58:33 +0000

    Copilot third commit - 3

--- comments.js
+++ comments.js
@@ -1,21 +1,12 @@
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
-// Solution: Use streams and pipe the data to the response object
-
-// Path: hello-world.js
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
-// Solution: Use streams and pipe the data to the response object
-
-// Path: index.js
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
-// Solution: Use streams and pipe the data to the response object
-
-// Path: index.js
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
-// Solution: Use streams and pipe the data to the response object
-
-// Path: index.js
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
-// Solution: Use streams and pipe the data to the response object
-
-// Path: index.js
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve
+// Create web server using Express.js
+const express = require('express');
+const app = express();
+const port = 3000;
+// Use static files
+app.use(express.static('public'));
+// Create route for comments
+app.get('/comments', (req, res) => {
+    res.send('Comments');
+});
+// Start server
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
